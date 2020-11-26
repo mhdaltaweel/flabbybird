53d42dcb923efaeee1fbbb18808e70911cf42711
leroybjj <leroy1tramp@gmail.com>
2020-11-26 09:44:11 +0000

    fonctions_fichiers.h ajout de la structure piece (test)

--- fonctions_fichiers.h
+++ fonctions_fichiers.h
@@ -1,5 +1,11 @@
 #ifndef FONCTIONS_FICHERS
 #define FONCTIONS_FICHERS
+#include <SDL2/SDL.h>
+#include "fonctions_SDL.h"
+
+struct piece_s {
+	
+};
 
 char** allouer_tab_2D(int n, int m);
 
@@ -14,5 +20,7 @@ char** lire_fichier(const char* nomFichier);
 char** modifier_caractere(char** tab, int n, int m, char ancien, char nouveau);
 
 void ecrire_fichier(const char* nomFichier, char** tab, int n, int m);
+
+//void liaisonSprite(SDL_Rect* Destr, char** tab, int m, int k, SDL_Renderer* renderer, SDL_Texture* texture, int x, int y);
 
 #endif
